df7d3cd6bf380a577c1dc0149b5fd3f1af9569e9
ThemeLove <lqsthemelove@163.com>
2018-02-24 14:06:08 +0800

    modify the vivo_plugin

--- UBSDK_VIVO/src/com/ubsdk/vivo/plugin/VIVOSDK.java
+++ UBSDK_VIVO/src/com/ubsdk/vivo/plugin/VIVOSDK.java
@@ -1,15 +1,28 @@
 package com.ubsdk.vivo.plugin;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+
+import org.json.JSONObject;
+
 import com.umbrella.game.ubsdk.UBSDK;
 import com.umbrella.game.ubsdk.bean.DataType;
 import com.umbrella.game.ubsdk.bean.UBOrderInfo;
 import com.umbrella.game.ubsdk.bean.UBRoleInfo;
 import com.umbrella.game.ubsdk.config.UBSDKConfig;
+import com.umbrella.game.ubsdk.http.UBHttpManager;
 import com.umbrella.game.ubsdk.listener.UBActivityListenerImpl;
+import com.umbrella.game.ubsdk.ui.UBLoadingDialog;
 import com.vivo.unionsdk.open.VivoExitCallback;
 import com.vivo.unionsdk.open.VivoPayCallback;
 import com.vivo.unionsdk.open.VivoPayInfo;
 import com.vivo.unionsdk.open.VivoUnionSDK;
+import com.yolanda.nohttp.NoHttp;
+import com.yolanda.nohttp.RequestMethod;
+import com.yolanda.nohttp.rest.OnResponseListener;
+import com.yolanda.nohttp.rest.Request;
+import com.yolanda.nohttp.rest.Response;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -29,8 +42,14 @@ public class VIVOSDK {
 	}
 	
 	private Activity mActivity;
+	private String mVIVO_StoreId;
+	private String mVIVO_AppId;
+	private UBLoadingDialog mUBLoadingDialog;
 	public void init(){
+		mVIVO_StoreId = UBSDKConfig.getInstance().getParamsMap().get("VIVO_STOREID");
+		mVIVO_AppId = UBSDKConfig.getInstance().getParamsMap().get("VIVO_APPID");
 		mActivity=UBSDKConfig.getInstance().getGameActivity();
+		mUBLoadingDialog = new UBLoadingDialog(mActivity, "订单创建中...");
 		UBSDK.getInstance().setUBActivityListener(new UBActivityListenerImpl(){
 
 			@Override
@@ -104,14 +123,71 @@ public class VIVOSDK {
 		
 	}
 	
-	public void pay(UBRoleInfo ubRoleInfo, UBOrderInfo ubOrderInfo) {
-		VivoPayInfo vivoPayInfo = new VivoPayInfo("商品名称","商品描述", "6.00","验签", "appid", "订单号", null);
-		VivoUnionSDK.pay(mActivity, vivoPayInfo, new VivoPayCallback() {
-			
+	public void pay(UBRoleInfo ubRoleInfo, final UBOrderInfo ubOrderInfo) {
+		String VIVOOrderUrl="https://pay.vivo.com.cn/vivoPay/getVivoOrderNum";
+		int VIVORequestWhat=1;
+		Request<String> VIVOOrderRequest = NoHttp.createStringRequest(VIVOOrderUrl,RequestMethod.POST);
+        //订单推送接口请在服务器端访问
+        final HashMap<String, String> params = new HashMap<String, String>();
+        params.put("notifyUrl",ubOrderInfo.getCallbackUrl());//回调地址
+        params.put("orderAmount",ubOrderInfo.getAmount()+"");  //注意：精确到小数点后两位；
+        params.put("orderDesc",ubOrderInfo.getGoodsDesc());
+        params.put("orderTitle",ubOrderInfo.getGoodsName());
+        SimpleDateFormat format = new SimpleDateFormat("yyyyMMddHHmmss");
+        params.put("orderTime",format.format(new Date()));//订单创建时间
+        params.put("storeId",mVIVO_StoreId);//商户ID
+        params.put("appId", mVIVO_AppId);                  //APPID
+//        params.put("storeOrder", UUID.randomUUID().toString().replaceAll("-", ""));//商户订单号
+        params.put("storeOrder",ubOrderInfo.getOrderId());//商户订单号
+        params.put("version", "1.0");
+        String str = VivoSignUtils.getVivoSign(params,mVIVO_StoreId);//signkey
+        params.put("signature", str);
+        params.put("signMethod", "MD5");
+		VIVOOrderRequest.add(params);
+		
+		UBHttpManager.getInstance().addRequest(VIVORequestWhat, VIVOOrderRequest, new OnResponseListener<String>() {
+
 			@Override
-			public void onVivoPayResult(String transNo, boolean isSuccess, String errorCode) {
-				
+			public void onFailed(int what, String arg1, Object arg2, Exception e, int arg4, long arg5) {
+				UBSDK.getInstance().getUBPayCallback().onFailed(ubOrderInfo.getCpOrderId(), "创建订单失败！", null);
+			}
+
+			@Override
+			public void onFinish(int what) {
+				mUBLoadingDialog.dismiss();
+			}
+
+			@Override
+			public void onStart(int what) {
+				mUBLoadingDialog.show();
+			}
+
+			@Override
+			public void onSucceed(int what, Response<String> response) {
+				try {
+					if (response.getHeaders().getResponseCode()==200) {
+						JSONObject jsob = new JSONObject(response.get());
+						String orderAmount = jsob.optString("orderAmount");
+						String vivoSignature = jsob.optString("vivoSignature");
+						String vivoOrder = jsob.optString("vivoOrder");
+						ubOrderInfo.setOrderId(vivoOrder);
+						VivoPayInfo vivoPayInfo = new VivoPayInfo(ubOrderInfo.getGoodsName(),ubOrderInfo.getGoodsDesc(),orderAmount,vivoSignature, mVIVO_AppId,vivoOrder, null);
+						VivoUnionSDK.pay(mActivity, vivoPayInfo, new VivoPayCallback() {
+							@Override
+							public void onVivoPayResult(String transNo, boolean isSuccess, String errorCode) {
+								if (isSuccess) {
+									UBSDK.getInstance().getUBPayCallback().onSuccess(ubOrderInfo.getOrderId(), ubOrderInfo.getCpOrderId(), ubOrderInfo.getExtrasParams());
+								}else{
+									UBSDK.getInstance().getUBPayCallback().onFailed(ubOrderInfo.getCpOrderId(), errorCode, null);
+								}
+							}
+						});
+					}
+				} catch (Exception e) {
+					
+				}
 			}
 		});
+		
 	}
 }
