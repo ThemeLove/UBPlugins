700e64dbd4298345b6f016d6266bbb608f049de0
ThemeLove <lqsthemelove@163.com>
2018-08-27 20:44:53 +0800

    替换UBSDK_AD_YYB中的为碰碰球正式参数

--- UBSDK_AD_YYB/src/com/ubsdk/ad/yyb/UBSDK_AD_YYB_MainActivity.java
+++ UBSDK_AD_YYB/src/com/ubsdk/ad/yyb/UBSDK_AD_YYB_MainActivity.java
@@ -1,5 +1,6 @@
 package com.ubsdk.ad.yyb;
 
+import com.tencent.tmgp.umbrella.boomdots.R;
 import com.umbrella.game.ubsdk.UBSDK;
 import com.umbrella.game.ubsdk.callback.UBADCallback;
 import com.umbrella.game.ubsdk.callback.UBExitCallback;
